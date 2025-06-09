38b9e7be48a41128783dfbaf90aca0e8cac892b7
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2025-06-09 13:56:08 -0300

    chore: adiciona script para arquivo limpo de anac_2025.cvs

--- utils/analise.py
+++ utils/analise.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df_anac = pd.read_csv('database/csv_anac_resumo_anual_2025.csv', sep=';', encoding='latin1')
+df_anac = pd.read_csv('database/csv_anac_2025.csv', sep=';', encoding='latin1')
 
 print(df_anac.info())
 
@@ -9,14 +9,16 @@ df_anac['HORAS VOADAS'] = pd.to_numeric(df_anac['HORAS VOADAS'], errors='coerce'
 df_anac['ANO'] = pd.to_datetime(df_anac['ANO'], errors='coerce')
 df_anac['MÊS'] = pd.to_datetime(df_anac['MÊS'], errors='coerce')
 
-# print('Quantidade de valores nulos em cada Serie/coluna:\n')
-# print(df_anac.isna().sum())
-# print('-------------------------------------------------')
+print('Quantidade de valores nulos em cada Serie/coluna:\n')
+print(df_anac.isna().sum())
+print('-------------------------------------------------')
 
-# print('Percentual de valores nulos por Serie/coluna:\n')
-# print(df_anac.isna().mean() * 100)
-# print('-------------------------------------------------')
+print('Percentual de valores nulos por Serie/coluna:\n')
+print(df_anac.isna().mean() * 100)
+print('-------------------------------------------------')
 
-df_anac = df_anac.dropna() # exclui registros que contém valores nulos e/ou NaN do DataFrame
+df_anac = df_anac.dropna()
 
 print(df_anac.info())
+
+df_anac.to_csv('database/csv_limpo_anac_2025.csv', sep=';', encoding='latin1')
