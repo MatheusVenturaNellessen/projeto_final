051626c96ac6847c0bd485ed5a3a8489466bd82b
André do Valle Ciccozzi <69083069+andre-ciccozzi@users.noreply.github.com>
2025-06-12 11:13:13 -0300

    atulizando

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import io
 import frontend.plane as pln
 import frontend.esqueleto as esq
 st.set_page_config(page_title="Dashboard Aviação", page_icon="✈️", layout="wide")
+
 if 'active_tab' not in st.session_state:
     st.session_state.active_tab = 'plane'
 
